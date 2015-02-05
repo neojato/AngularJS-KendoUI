37607ffbc757ee56f4c6c6691117339db2c4ccef
neojato <kpaul86+github@gmail.com>
2015-02-05 00:56:42 -0600

    clean up tabs & lint fixes

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-var app = angular.module('address-book', ['kendo-directives']);
+/* global angular, kendo, console */
+(function () {
+    'use strict';
 
-app.controller('ListCtrl', ['$scope','$http', function($scope, $http){
-  $scope.autocompleteOptions = {
-    dataTextField: 'name',
-    change: function() {
-      $scope.filter = $scope.search.value();
-    }
-  };
-}]);
+    var app = angular.module('address-book', ['kendo.directives']);
+
+    app.controller('ListCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.letters = [];
+
+        $http.get('contacts.json').success(function (response) {
+            $scope.contacts = angular.forEach(response, function (contact) {
+                // add unique letters to alphabet array
+                if ($scope.letters.indexOf(contact.first_name[0]) === -1) {
+                    $scope.letters.push(contact.first_name[0]);
+                }
+                contact.dob = kendo.toString(new Date(contact.dob), 'dd/MM/yyyy');
+                contact.name = contact.first_name + ' ' + contact.last_name;
+
+                return contact;
+            });
+
+            $scope.letters.sort();
+        });
+
+        $scope.autocompleteOptions = {
+            dataTextField: 'name',
+            change: function () {
+                $scope.filter = $scope.search.value();
+            }
+        };
+    }]);
+})();
